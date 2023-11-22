1f93937e655efc1b340571089527e856e8c752bb
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-22 10:13:57 -0500

    added capiton options

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by Christopher Lamm on 11/22/23.
+//
+
+import Foundation
+
+struct CaptionOption {
+    let emoji: String
+    let caption: String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let topCaptionChoices = [
+        CaptionOption(emoji: "🕶️", caption: "You know what's cool?"),
+        CaptionOption(emoji: "💥", caption: "You know what makes me mad?"),
+        CaptionOption(emoji: "💕", caption: "You know what I love?"),
+    ]
+    
+    let bottomCaptionChoices = [
+        CaptionOption(emoji: "🐱", caption: "Cats wearing hats."),
+        CaptionOption(emoji: "🐕", caption: "Dogs carrying logs."),
+        CaptionOption(emoji: "🐒", caption: "Monkeys being funky.")
+    ]
 
     @IBOutlet weak var topSegmentedControl: UISegmentedControl!
     @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
@@ -22,7 +33,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
